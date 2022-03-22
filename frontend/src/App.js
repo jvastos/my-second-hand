3453a43b6514430cf8509d47c79dca014f2a25ca
João Almeida <jvastos@gmail.com>
2022-03-22 16:24:06 +0100

    working on retrieving and presenting the cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 
 function App() {
 
-async function createCart(url) {
+const baseURL = "http://localhost:8080/my-second-hand/carts";
+
+const [cart, setCart] = useState("cart initiated");  
+
+/* useEffect(() => {
+  setCart("cart changed by useEffect");
+}, []) */
+
+async function createCart() {
 
     console.log("New cart created");
 
-    const response = await fetch(url, {
+    const response = await fetch(baseURL, {
       method: 'POST',
       mode: 'cors',
       cache: 'no-cache', 
@@ -23,11 +32,25 @@ async function createCart(url) {
 
   }
 
-async function updateCart(url) {
+async function getCart() {
+
+    const response = await fetch(baseURL + "/id/2022313285");
+    let responseFormat = await response.json();
+    setCart(responseFormat[0].productsList.map(i => <li key={Math.random()}> {"name: " + i.name.toString() + " " + "quantity: " + i.quantity.toString()}</li>));
+    console.log(responseFormat[0].productsList.map(i => i.name + " x " + i.quantity.toString()));
+
+  }
+
+  /* function test() {
+    setCart("cart changed by test function");
+    console.log(cart);
+  } */
+
+async function updateCart() {
 
     console.log("Cart updated");
 
-    const response = await fetch(url, {
+    const response = await fetch(baseURL + "/id/2022313285?name=prod4&quantity=1", {
       method: 'PATCH',
       mode: 'cors',
       cache: 'no-cache', 
@@ -45,8 +68,12 @@ async function updateCart(url) {
 
   return (
     <div>
-     <button onClick={() => createCart("http://localhost:8080/my-second-hand/carts")}>Create cart</button>
-     <button onClick={() => updateCart("http://localhost:8080/my-second-hand/carts/id/2022313285?name=prod4&quantity=1")}>Update cart</button>
+      <button onClick={createCart}>Create cart</button>
+      <button onClick={() => getCart("http://localhost:8080/my-second-hand/carts/id/2022313285")}>Get cart</button>
+      <button onClick={updateCart}>Update cart</button>
+      <ul>
+        {cart}
+      </ul>
     </div>
   );
 }
