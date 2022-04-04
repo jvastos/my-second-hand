c0d58f3d79d6ea2e3e8d72fb8d454a355c6b5236
João Almeida <jvastos@gmail.com>
2022-04-04 10:49:41 +0200

    products to Products after ignorecase config

---
+++ frontend/src/components/Products.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useState, useEffect } from "react"
+import AddToCartButton from "./AddToCartButton";
+import { API_BASE_URL } from "../config";
+    
+function Products(props) {
+
+    const baseURLproducts = `${API_BASE_URL}/products`
+    const [products, setProducts] = useState("no products");
+
+
+    useEffect(()=>{
+        async function fetchProducts() {
+            await fetch(!props.termToBeSearched ? `${baseURLproducts}`:`${baseURLproducts}/search?name=${props.termToBeSearched}`)
+            .then(response => response.json())
+            .then(data => setProducts(data.map(i => 
+                <div 
+                key={Math.random()} 
+                className="col-lg-4">
+                    <div className="card text-center text-dark bg-light m-3">
+                        <h3 className="card-title m-3">{i.name}</h3>
+                        <h3 className="card-subtitle">{i.price}$</h3>
+                        <div className="row justify-content-around">
+                            <AddToCartButton
+                            setCartContent={props.setCartContent}
+                            cartContent={props.cartContent} 
+                            prodname={i.name}
+                            prodprice={i.price}/>
+                        </div>
+                    </div>
+                </div>
+            )))
+            .catch (error => {console.log("There was an error fecthing the products", error)});
+        }
+        fetchProducts();
+    }, [props, baseURLproducts]);
+
+    return products;
+}
+
+export default Products
